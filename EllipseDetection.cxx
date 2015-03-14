ca8388e7206219c36f43883d21728b3611782d8a
JimmyCai91 <caijinzhengcn@gmail.com>
2015-03-14 12:52:36 -0400

    almost

--- EllipseDetection.cxx
+++ EllipseDetection.cxx
@@ -6,6 +6,7 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #include <iterator>
+#include <ctime>
 #include <algorithm>
 
 #define min_majoraxis 20 
@@ -26,66 +27,82 @@ int main() {
 	Mat edgemap = preprocessing( image );
     cvtColor(image, image, CV_GRAY2BGR);
 
+
+    cout << "Edge map dilation..." << endl;
     dilate(edgemap, edgemap, Mat());
     imshow("Edge Map", edgemap);
-    cout << "Edge map dilation" << endl;
-    waitKey(0);
+    waitKey(200);
 
     Mat mask = Mat::zeros(edgemap.rows+2, edgemap.cols+2, CV_8U);
+    cout << "Mask..." << endl;
     floodFill(edgemap, mask, Point(1,1), 255, 0, Scalar(), Scalar(), 4+(255<<8) + FLOODFILL_MASK_ONLY);
+    imshow("Edge Map", mask);
+    waitKey(200);
+
+    cout<< "Closing operation..." << endl;
     dilate(mask, mask, Mat());
     erode(mask, mask, Mat());
+    imshow("Edge Map", mask);
+    waitKey(200);
+
+    cout << "Edge map result..." << endl;
     dilate(mask, edgemap, Mat());
     edgemap = edgemap - mask;
-
     imshow("Edge Map", edgemap);
-    waitKey(0);
+    waitKey(200);
 
     Mat rgb;
     cvtColor(edgemap, rgb, CV_GRAY2BGR);
 
-	// get edge points
+    cout << "Collecting edge points..." << endl;
 	vector<CvPoint> edgepoints = findedgepoints( edgemap );
 
-	// get pairwise point distance
-	Mat dist = fastpdist( edgepoints );
+    cout << "Edge points randomize..." << endl;
+    random_shuffle(edgepoints.begin(),edgepoints.end());
 
-	vector<int> accumulator, copy_accumulator;
-	for(int i=0; i<edgepoints.size(); i++)
+    cout << "Calculation distance map..." << endl;
+	Mat dist = fastpdist( edgepoints );
+    dist.convertTo(dist,CV_32S);
+
+	vector<int> accumulator;
+    Point centerpoint;
+    double majoraxis, minoraxis;
+    double f, d, costau, costau2, sintau2, angle;
+    int i, j, k, kk;
+    clock_t t;
+    t = clock();
+	for(i=0; i<edgepoints.size(); ++i)
 	{
-        for(int j=0; j<edgepoints.size(); j++)
+        for(j=i+1; j<edgepoints.size(); ++j)
 		{
-            if(dist.at<double>(i,j) < min_majoraxis)
+            if(dist.at<int>(i,j) < min_majoraxis)
                 continue;
             
-            if(dist.at<double>(i,j) > max_majoraxis)
+            if(dist.at<int>(i,j) > max_majoraxis)
                 continue;
 
-            CvPoint centerpoint = cvPoint((edgepoints[i].x+edgepoints[j].x)/2,(edgepoints[i].y+edgepoints[j].y)/2); 	
+            centerpoint = cvPoint((edgepoints[i].x+edgepoints[j].x)/2,(edgepoints[i].y+edgepoints[j].y)/2); 	
 
             /// start voting 
-            double majoraxis = pointdist(edgepoints[i],edgepoints[j])/2;
-            double minoraxis;
+            majoraxis = pointdist(edgepoints[i],edgepoints[j])/2;
+            minoraxis;
             accumulator.resize(ceil(majoraxis));
-			for( int k=0; k<edgepoints.size(); k++ )
+			for(k=0; k<edgepoints.size(); ++k)
 			{
                 if(pointdist(centerpoint,edgepoints[k]) >= majoraxis)
                     continue;
-               
-                double f = min(pointdist(edgepoints[i],edgepoints[k]),pointdist(edgepoints[j],edgepoints[k]));
+
+                f = min(pointdist(edgepoints[i],edgepoints[k]),pointdist(edgepoints[j],edgepoints[k]));
                 if(f < min_minoraxis)
                     continue;
-
-                double d = pointdist(centerpoint,edgepoints[k]);
-
-				double costau = (majoraxis*majoraxis+d*d-f*f)/(2*majoraxis*d);
+                
+                d = pointdist(centerpoint,edgepoints[k]);
+				costau = (majoraxis*majoraxis+d*d-f*f)/(2*majoraxis*d);
                 if(costau >= 1)
                     continue;
 
-				double costau2 = costau*costau;
-
-				double sintau2 = 1-costau2;
-
+				costau2 = costau*costau;
+                sintau2 = 1-costau2;
 				minoraxis = sqrt((majoraxis*majoraxis*d*d*sintau2)/(majoraxis*majoraxis-d*d*costau2));
                 if(minoraxis < min_minoraxis)
                     continue;
@@ -95,12 +112,12 @@ int main() {
    
             if (*max_element(accumulator.begin(),accumulator.end()) > least_number)
 			{
-                cout << "accumulator's size is: " << accumulator.size() << endl;
-                for(vector<int>::const_iterator p = accumulator.begin(); p != accumulator.end(); ++p)
-                    cout << *p << ' ';
-                cout << endl;
+//                cout << "accumulator's size is: " << accumulator.size() << endl;
+//                for(vector<int>::const_iterator p = accumulator.begin(); p != accumulator.end(); ++p)
+//                    cout << *p << ' ';
+//                cout << endl;
  
-                double angle = atan2( (edgepoints[i].y - edgepoints[j].y),0.000001+(edgepoints[i].x - edgepoints[j].x) ) * 180 / 3.14;
+                angle = atan2( (edgepoints[i].y - edgepoints[j].y),0.000001+(edgepoints[i].x - edgepoints[j].x) ) * 180 / 3.14;
                 minoraxis = distance(accumulator.begin(), max_element(accumulator.begin(), accumulator.end()));
                 cout << "end point " << i << " " << edgepoints[i].x << " " << edgepoints[i].y << endl;
                 cout << "end point " << j << " " << edgepoints[j].x << " " << edgepoints[j].y << endl;
@@ -116,24 +133,17 @@ int main() {
                 waitKey(1);
                 
                 // clear out this ellipse
-                vector<int> points2move;
-                for(int k=0; k<edgepoints.size(); k++)
-                    if(dist.at<double>(i,k) <= 1.5*majoraxis || dist.at<double>(j,k) <= 1.5*majoraxis)
-                    {
-                        dist.at<double>(i,k) = max_majoraxis+1;
-                        dist.at<double>(j,k) = max_majoraxis+1;
-                        points2move.push_back(k);
-                    }
-                cout<< points2move.size() << " points to move." << endl;
-                for(int k=0; k<points2move.size(); k++)
-                    for(int kk=0; kk<edgepoints.size(); kk++)
-                        dist.at<double>(kk,points2move[k]) = max_majoraxis+1;
+                for(k=0; k<edgepoints.size(); ++k)
+                    if(dist.at<int>(i,k) <= 1.5*majoraxis || dist.at<int>(j,k) <= 1.5*majoraxis)
+                        for(kk=0; kk<edgepoints.size(); ++kk)
+                            dist.at<int>(kk,k) = max_majoraxis+1;
 
 			}
             accumulator.clear();
         }
 	}
-
+    t = clock() - t;
+    cout << "It took " << (float)t/CLOCKS_PER_SEC << " seconds..." << endl;  
     imshow("Edge Map", image);
     cout <<"Press any key to exit..."<< endl;
     waitKey(0);
@@ -141,7 +151,7 @@ int main() {
 	return 0;
 }
 
-double pointdist( CvPoint point1, CvPoint point2 )
+inline double pointdist( CvPoint point1, CvPoint point2 )
 {
 	double dist;
 	double p1x, p1y, p2x, p2y;
