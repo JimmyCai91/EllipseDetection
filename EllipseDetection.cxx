fba6f4414342e7616bc506ee55420d8ada59b670
JimmyCai91 <caijinzhengcn@gmail.com>
2015-03-14 13:50:44 -0400

    finish

--- EllipseDetection.cxx
+++ EllipseDetection.cxx
@@ -27,7 +27,6 @@ int main() {
 	Mat edgemap = preprocessing( image );
     cvtColor(image, image, CV_GRAY2BGR);
 
-
     cout << "Edge map dilation..." << endl;
     dilate(edgemap, edgemap, Mat());
     imshow("Edge Map", edgemap);
@@ -64,52 +63,46 @@ int main() {
 	Mat dist = fastpdist( edgepoints );
     dist.convertTo(dist,CV_32S);
 
-	vector<int> accumulator;
+	vector<long> accumulator;
+    accumulator.resize(max_majoraxis);
     Point centerpoint;
     double majoraxis, minoraxis;
     double f, d, costau, costau2, sintau2, angle;
     int i, j, k, kk;
+    int num_edgepoints = edgepoints.size();
     clock_t t;
     t = clock();
-	for(i=0; i<edgepoints.size(); ++i)
+	for(i=0; i<num_edgepoints; ++i)
 	{
-        for(j=i+1; j<edgepoints.size(); ++j)
+        for(j=i+1; j<num_edgepoints; ++j)
 		{
             if(dist.at<int>(i,j) < min_majoraxis)
                 continue;
-            
             if(dist.at<int>(i,j) > max_majoraxis)
                 continue;
-
             centerpoint = cvPoint((edgepoints[i].x+edgepoints[j].x)/2,(edgepoints[i].y+edgepoints[j].y)/2); 	
 
             /// start voting 
             majoraxis = pointdist(edgepoints[i],edgepoints[j])/2;
-            minoraxis;
-            accumulator.resize(ceil(majoraxis));
-			for(k=0; k<edgepoints.size(); ++k)
+			for(k=0; k<num_edgepoints; ++k)
 			{
                 if(pointdist(centerpoint,edgepoints[k]) >= majoraxis)
                     continue;
-
                 f = min(pointdist(edgepoints[i],edgepoints[k]),pointdist(edgepoints[j],edgepoints[k]));
                 if(f < min_minoraxis)
                     continue;
-                
                 d = pointdist(centerpoint,edgepoints[k]);
 				costau = (majoraxis*majoraxis+d*d-f*f)/(2*majoraxis*d);
                 if(costau >= 1)
                     continue;
-
 				costau2 = costau*costau;
                 sintau2 = 1-costau2;
 				minoraxis = sqrt((majoraxis*majoraxis*d*d*sintau2)/(majoraxis*majoraxis-d*d*costau2));
                 if(minoraxis < min_minoraxis)
                     continue;
-
 				if(minoraxis < majoraxis) accumulator[round(minoraxis)]++;						
 			}
-   
+            
             if (*max_element(accumulator.begin(),accumulator.end()) > least_number)
 			{
 //                cout << "accumulator's size is: " << accumulator.size() << endl;
@@ -117,29 +110,29 @@ int main() {
 //                    cout << *p << ' ';
 //                cout << endl;
  
-                angle = atan2( (edgepoints[i].y - edgepoints[j].y),0.000001+(edgepoints[i].x - edgepoints[j].x) ) * 180 / 3.14;
+                angle = atan2((edgepoints[i].y-edgepoints[j].y),0.001+(edgepoints[i].x-edgepoints[j].x))*57;
                 minoraxis = distance(accumulator.begin(), max_element(accumulator.begin(), accumulator.end()));
-                cout << "end point " << i << " " << edgepoints[i].x << " " << edgepoints[i].y << endl;
-                cout << "end point " << j << " " << edgepoints[j].x << " " << edgepoints[j].y << endl;
-                cout << "majoraxis: " << majoraxis << ", minoraxis: " << minoraxis << ", angle: " 
-                        << angle << ", match points: " << *max_element(accumulator.begin(), accumulator.end()) << endl;
+//                cout << "end point " << i << " " << edgepoints[i].x << " " << edgepoints[i].y << endl;
+//                cout << "end point " << j << " " << edgepoints[j].x << " " << edgepoints[j].y << endl;
+//                cout << "majoraxis: " << majoraxis << ", minoraxis: " << minoraxis << ", angle: " 
+//                        << angle << ", match points: " << *max_element(accumulator.begin(), accumulator.end()) << endl;
 
                 // mark end points
-                circle(rgb, edgepoints[i], 5, Scalar(255,0,0));
-                circle(rgb, edgepoints[j], 5, Scalar(255,0,0));
-                ellipse(rgb, centerpoint, Size(majoraxis,minoraxis), angle, 0, 360, Scalar(0,255,0), 1);
+//                circle(rgb, edgepoints[i], 3, Scalar(255,0,0));
+//                circle(rgb, edgepoints[j], 3, Scalar(255,0,0));
+//                ellipse(rgb, centerpoint, Size(majoraxis,minoraxis), angle, 0, 360, Scalar(0,255,0), 1);
                 ellipse(image, centerpoint, Size(majoraxis,minoraxis), angle, 0, 360, Scalar(0,225,0), 2);
-                imshow("Edge Map", rgb);
-                waitKey(1);
+//                imshow("Edge Map", rgb);
+//                waitKey(1);
                 
                 // clear out this ellipse
-                for(k=0; k<edgepoints.size(); ++k)
+                for(k=0; k<num_edgepoints; ++k)
                     if(dist.at<int>(i,k) <= 1.5*majoraxis || dist.at<int>(j,k) <= 1.5*majoraxis)
-                        for(kk=0; kk<edgepoints.size(); ++kk)
+                        for(kk=0; kk<num_edgepoints; ++kk)
                             dist.at<int>(kk,k) = max_majoraxis+1;
 
 			}
-            accumulator.clear();
+            accumulator.assign(max_majoraxis,0);
         }
 	}
     t = clock() - t;
